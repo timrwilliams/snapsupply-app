164d20309deef7288b454e78281ba2339e538006
Tim Williams <timwemail@gmail.com>
2013-07-02 14:45:52 +0100

    Add local storage to calendar data

--- www/js/AvailabilityView.js
+++ www/js/AvailabilityView.js
@@ -16,63 +16,97 @@ var AvailabilityView = function() {
         	defaultDate:null,multiSelect: 99, monthsToShow: 1, monthsToStep: 1,
         	onMultiSelect:onDateChange, yearRange: '-1:+1'});
 
-        	if(localStorage.getItem(LS_CALENDAR_KEY)){
-        	    storedDates =JSON.parse(localStorage.getItem(LS_CALENDAR_KEY));
-        	    selectedDates = new Array();
-        	    var length = storedDates.length,
-                element = null;
-                for (var i = 0; i < length; i++) {
-                  element = storedDates[i];
-                  if(element!=null){
-                    selectedDates.push(new Date(element));
-                  }
-                }
-        	}
-        	else{
-                localStorage.setItem(LS_CALENDAR_KEY,JSON.stringify(selectedDates));
-        	}
+
+        	this.loadDates();
         	if(selectedDates.length>0){
     	        $('#multiInlinePicker').datepick('setDate', selectedDates);
     	        $('#multiInlinePicker').datepick({defaultDate: null});
     	    }
     }
 
+    this.loadDates = function(){
+        dateSync();
+        if(localStorage.getItem(LS_CALENDAR_KEY)){
+            storedDates =JSON.parse(localStorage.getItem(LS_CALENDAR_KEY));
+            selectedDates = new Array();
+            var length = storedDates.length,
+            element = null;
+            for (var i = 0; i < length; i++) {
+                element = storedDates[i];
+                if(element!=null){
+                    selectedDates.push(new Date(element));
+                }
+            }
+        }
+        else{
+            localStorage.setItem(LS_CALENDAR_KEY,JSON.stringify(selectedDates));
+        }
+    }
+
 	this.initialize();
 
     function  saveDateChange(direction,date) {
-        /*dateStr = date.toJSON().substring(0,10);
-        baseUrl = http://localhost:3000/
-                         $.ajax({
-                 		    url: 'http://localhost:3000/diary.jsonp',
-                 		    dataType: 'jsonp',
-                            jsonp: 'callback',
-                            crossDomain: true,
-                 		    timeout: 25000,
-                 		    data : {
-                                 auth_token : localStorage.getItem('auth_token'),
-                                 direction : direction,
-                                 date: dateStr
-                             },
-                 		    success: function(data, status) {
-                 			   alert(data);
-                 		    },
-                 		    error: function(){
-                     			alert('Oh no! not again.');
-                 	    	}
-                 	});
-*/
-                     };
+        console.log("Sending data to server");
+        dateStr = date.toJSON().substring(0,10);
+        $.ajax({
+            url: baseUrl+'/diaries/2/diary_entries/create.jsonp',
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            crossDomain: true,
+            timeout: 25000,
+            data : {
+                 auth_token : localStorage.getItem('auth_token'),
+                 diary_entry : {
+                    date: dateStr,
+                    entry_type:1
+                 }
+             },
+            success: function(data, status) {
+               $('#calendar_status').text("Changes saved");
+               console.log("succesfully saved");
+            },
+            error: function(){
+            	alert('Oh no! not again.');
+            }
+        });
+    };
+
+    function  saveDateRemoval(date) {
+            console.log("Sending removal data to server");
+            dateStr = date.toJSON().substring(0,10);
+            $.ajax({
+                url: baseUrl+'/diaries/2/diary_entries/destroy.jsonp',
+                dataType: 'jsonp',
+                jsonp: 'callback',
+                crossDomain: true,
+                timeout: 25000,
+                data : {
+                     auth_token : localStorage.getItem('auth_token'),
+                     diary_entry : {
+                        date: dateStr,
+                        entry_type:1
+                     }
+                 },
+                success: function(data, status) {
+                   $('#calendar_status').text("Changes saved");
+                   console.log("Destroy successfully saved");
+                },
+                error: function(){
+                	alert('Oh no! not again.');
+                }
+            });
+        };
 
     function onDateChange(direction,date){
         $('calendar_status').text("Saving changes...");
         if(direction=="ADD"){
+            saveDateChange(direction,date);
             addDate(date);
         }
         else{
+            saveDateRemoval(date);
             removeDate(date);
         }
-        saveDateChange(direction,date);
-        $('#calendar_status').text("Changes saved");
 	};
 
     function addDate(date){
@@ -104,8 +138,30 @@ var AvailabilityView = function() {
         }
     }
 
+    function dateSync(){
+        $.ajax({
+                url: baseUrl+'/diaries/2/diary_entries.jsonp',
+                dataType: 'jsonp',
+                jsonp: 'callback',
+                crossDomain: true,
+                timeout: 25000,
+                data : {
+                     auth_token : localStorage.getItem('auth_token')
+                 },
+                success: function(data, status) {
+                   console.log("succesfully retrieved json");
+                   console.log(data.entries);
+                },
+                error: function(){
+                	alert('Oh no! not again.');
+                }
+            });
+    };
+
     var selectedDates = new Array();
     var LS_CALENDAR_KEY = 'calendar.dates';
+    //baseUrl = "http://localhost:3000";
+    baseUrl = "http://snapsupply.herokuapp.com";
 };
 
 
