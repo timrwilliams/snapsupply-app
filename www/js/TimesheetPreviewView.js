2a14439d550c891b6fd63e5588fcdab30b3e3807
Tim Williams <tim@teachmatic.com>
2013-08-13 14:44:27 +0100

    Fix description and make timesheet more dynamic.

--- www/js/TimesheetPreviewView.js
+++ www/js/TimesheetPreviewView.js
@@ -1,8 +1,17 @@
 var TimesheetPreviewView = function() {
+    var daysOfWeek=new Array("monday","tuesday","wednesday","thursday","friday","saturday","sunday");
 
 	this.initialize = function() {
         $( "#ts-pv-submit-btn" ).bind( "click", this.onCreate);
-        $( "#ts-pv-cancel-btn" ).bind( "click", this.onCancel);        
+        $( "#ts-pv-cancel-btn" ).bind( "click", this.onCancel);      
+        for (var i = 0, len = daysOfWeek.length; i < len; i++) {
+            var day = daysOfWeek[i];
+            if (tsForm.days[day]) {
+                var preview = day.capitalize() + " " + tsForm.days[day];
+                $("#data-preview").append('<li>'+preview+'</li>');
+            }
+        };
+        $("#data-preview").listview('refresh');
     };
 
      this.onCreate = function(event, ui) {
