bf5e133f4b1e25ed58f253d9f227f1a37f7f7f43
Tim Williams <tim@teachmatic.com>
2013-11-13 11:24:10 +0000

    Add ios to build scripts

--- bin/setup_client.js
+++ bin/setup_client.js
@@ -2,21 +2,30 @@
 var path = require( "path" ),
     fs = require( "fs" ),
     shell = require( "shelljs" ),
-    rootdir = "C:/dev/projects/snapsupply-app";
+    rootdir = "/Users/siriley/dev/snapsupply-app";
 
 function copy_client_assets(client){
+    setup_android(rootdir);
+    setup_ios(rootdir);
+    setup_plugins(rootdir);
     var client_dir = "";
     if(client){
         console.log("Building for client: "+client);
         client_dir = client;
     }
+    else{
+        console.log("No client set, please set the $CLIENT environment variable");
+        console.log("export CLIENT=");
+        process.exit(1);
+    }
+
     var	client_asset_dir = rootdir + "/clients/" + client_dir;
     var clientroot = rootdir + "/gen/" + client_dir;
     try {
         fs.lstatSync( clientroot ).isDirectory();
     }
     catch( e ) {	
-        console.log( "android platform does not exist. Creating at:"+clientroot );
+        console.log( "This client does not exist yet. Creating at:"+clientroot );
         shell.mkdir("-p",clientroot);
     }
 
@@ -25,12 +34,51 @@ function copy_client_assets(client){
     shell.cp( "-Rf", rootdir + "/www" , clientroot + "/");
     shell.cp( "-Rf", rootdir + "/platforms" , clientroot + "/");
     shell.cp( "-Rf", rootdir + "/plugins" , clientroot + "/");
-    shell.cp( "-Rf", client_asset_dir + "/www/config.xml" , clientroot + "/www/config.xml" + "/");
+    shell.cp( "-Rf", client_asset_dir + "/www/config.xml" , clientroot + "/www/config.xml");
     shell.rm( "-Rf", clientroot + "/platforms/android/src/com/teachmatic");
     shell.cp( "-Rf", client_asset_dir + "/android/AndroidManifest.xml", clientroot + "/platforms/android/AndroidManifest.xml");
     shell.cp( "-Rf", client_asset_dir + "/android/src/com/teachmatic/snapsupply", clientroot + "/platforms/android/src/com/teachmatic");
-
+    shell.cp( "-Rf", client_asset_dir + "/ios/SnapSupply-Info.plist", clientroot + "/platforms/ios/SnapSupply");
     console.log( "Generated client folder." );
+}
+
+function setup_plugins(rootdir){
+    console.log("Checking plugins...");
+    pluginroot = rootdir + "/plugins";
+    plugins = ["geolocation","console","splashscreen"];
+    for (i = 0;i<plugins.length;i++){
+        plugindir = pluginroot + "/" + plugins[i]; 
+        try {
+            fs.lstatSync( plugindir ).isDirectory();
+        }
+        catch( e ) {    
+            console.log( plugins[i] + " plugin does not exist yet. Adding..." );
+            shell.exec("cordova plugin add org.apache.cordova."+plugins[i]);
+        }
+    }
+}
+
+function setup_android(rootdir){
+    androidroot = rootdir + "/platforms/android";
+    try {
+        fs.lstatSync( androidroot ).isDirectory();
+    }
+    catch( e ) {    
+        console.log( "android platform does not exist yet. Creating at:"+androidroot );
+        shell.exec("cordova platform add android");
+    }
+}
+
+
+function setup_ios(rootdir){
+    iosroot = rootdir + "/platforms/ios";
+    try {
+        fs.lstatSync( iosroot ).isDirectory();
+    }
+    catch( e ) {    
+        console.log( "ios platform does not exist yet. Creating at:"+iosroot );
+        shell.exec("cordova platform add ios");
+    }
 }
 
 exports.copy_client_assets = copy_client_assets;
